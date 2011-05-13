a801a064a2053fb9e1dcb11e5ef243c77419625b
Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
2011-05-13 08:14:08 +0000

    Merge Iblis' changes

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>?ver=<?php $file = dirname(__FILE__) . '/style.css'; echo filemtime($file);?>" type="text/css" media="screen" />
 		<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/handheld.css?ver=<?php $file = dirname(__FILE__) . '/handheld.css'; echo filemtime($file);?>" type="text/css" media="handheld" />
-		<!--[if IE 8]><link rel="stylesheet" href="<?php bloginfo('template_directory')?>/IE.css?ver=<?php $file = dirname(__FILE__) . '/IE8.css'; echo filemtime($file);?>" type="text/css" /><![endif]-->
+		<!--[if IE 8]><link rel="stylesheet" href="<?php bloginfo('template_directory')?>/IE8.css?ver=<?php $file = dirname(__FILE__) . '/IE8.css'; echo filemtime($file);?>" type="text/css" /><![endif]-->
 		<!--[if IE 7]><link rel="stylesheet" href="<?php bloginfo('template_directory')?>/IE7.css?ver=<?php $file = dirname(__FILE__) . '/IE7.css'; echo filemtime($file);?>" type="text/css" /><![endif]-->
 		<!--[if IE 6]><link rel="stylesheet" href="<?php bloginfo('template_directory')?>/IE6.css?ver=<?php $file = dirname(__FILE__) . '/IE6.css'; echo filemtime($file);?>" type="text/css" /><![endif]-->
 		<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
@@ -36,7 +36,6 @@
 		<div id="header" class="clear">
 			<span id="top"></span>
 			<?php get_sidebar(); ?>
-			<?php wp_nav_menu(  array( 'menu' => 'hsnu-menu', 'container' => '', 'menu_class' => 'clear menu inset' ) );//wp_list_pages('title_li=&depth=1'); ?>
 			<script type='text/javascript' src="<?php bloginfo('template_directory')?>/js/effects.js"></script>
 			<script type="text/javascript">
 				/* <![CDATA[ */
@@ -48,4 +47,6 @@
 				dropShadows: false 
 				 }); });
 			</script>
-			</div><!--header-->
+			</div>
+			<?php wp_nav_menu(  array( 'menu' => 'hsnu-menu', 'container' => '', 'menu_class' => 'clear menu inset' ) );//wp_list_pages('title_li=&depth=1'); ?>
+			<!--header-->
