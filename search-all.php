67c386e7c35f9bb4415cf4e0504ae55318ac37cf
Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
2012-07-18 04:12:03 +0000

    Add feature to search messages by owner

--- search-all.php
+++ search-all.php
@@ -4,37 +4,45 @@
  */
 global $wpdb;
 
-if ( isset($_GET['sq']) && isset($_GET['st']) && !empty($_GET['st']) && !empty($_GET['sq']) )
+// If no search type is specified, show search form
+if ( isset($_GET['st']) && !empty($_GET['st']) )
 {
 	$st = $wpdb->escape($_GET['st']);
-	$sq = $wpdb->escape($_GET['sq']);
-	
+	$sq = $wpdb->escape($_GET['sq']);// search query
+	$owner_query = $wpdb->escape($_GET['owner']);
+
+	// Is this WP search or BT search?
 	if ( $st == 'bt' )
 	{
 		get_header();
-		$sp = (empty($_GET['sp'])) ? '' : $wpdb->escape($_GET['sp']);
+		$sp = (empty($_GET['sp'])) ? '' : $wpdb->escape($_GET['sp']); // sp = search page
 		if ( !$sp ) $sp = 1;
 ?>
-			<div class="content-box clear" >
-				<div class="content">
-				<?php do_action('get_bt_search',$sq,$sp); ?>
-				</div><!--content-->
-			</div><!--content-box-->
-			<?php  get_footer(); 
+<div class="content-box clear" >
+	<div class="content">
+<?php
+		// Search by title or by owner?
+		if ( isset($_GET['sq']) ) {
+			do_action('get_bt_search_by_title',$sq,$sp);
+		} elseif ( isset($_GET['owner']) ) {
+			do_action('get_bt_search_by_owner', $owner_query, $sp);
+		}
+		?>
+	</div><!--content-->
+</div><!--content-box-->
+<?php  get_footer(); 
 	} elseif ( $st == 'wp' ) {
 		// Show WP's search result
 		$bg_url = get_bloginfo('wpurl');
 		header("Location: $bg_url/?s=$sq");
-		die;
+die;
 	}
-}
-else 
-{
+} else {
 	get_header();?>
-		<div class="content-box clear" >
-			<div class="content">
-			<?php get_search_form(); ?>
-			</div><!--content-->
-		</div><!--content-box-->
+<div class="content-box clear" >
+	<div class="content">
+		<?php get_search_form(); ?>
+	</div><!--content-->
+</div><!--content-box-->
 <?php get_footer();
 } ?>
--- search.php
+++ search.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
-			<div class="content-box" >
-				<div class="content">
-				<?php #if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
-				<h2>搜尋結果：<?php the_search_query(); ?></h2>
-				<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-				<div id="nav-above">
-				<span class="nav-previous"><?php next_posts_link( '&larr;上一頁' ); ?></span>
-				<span class="nav-next"><?php previous_posts_link( '下一頁&rarr;' ); ?></span>
-				</div>
-				<?php endif; 
-				if ( have_posts() ) {
-					while ( have_posts() ) { 
-						the_post(); ?>
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h3>
-				<div class="entry-content-search"><?php the_excerpt(); ?></div>
-				<?php		
-					}	
-				}
-				if ( $wp_query->max_num_pages > 1 ) : ?>
-				<div id="nav-bottom">
-				<span class="nav-previous"><?php next_posts_link( '&larr;上一頁' ); ?></span>
-				<span class="nav-next"><?php previous_posts_link( '下一頁&rarr;' ); ?></span>
-				</div>
-				<?php endif; ?>
-				</div><!--content-->
-			</div><!--content-box-->
+<div class="content-box" >
+	<div class="content">
+		<?php #if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+		<h2>搜尋結果：<?php the_search_query(); ?></h2>
+		<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+		<div id="nav-above">
+			<span class="nav-previous"><?php next_posts_link( '&larr;上一頁' ); ?></span>
+			<span class="nav-next"><?php previous_posts_link( '下一頁&rarr;' ); ?></span>
+		</div>
+<?php endif; 
+if ( have_posts() ) {
+	while ( have_posts() ) { 
+		the_post(); ?>
+		<h3><a href="<?php the_permalink(); ?>"><?php the_title()?></a></h3>
+		<div class="entry-content-search"><?php the_excerpt(); ?></div>
+<?php		
+	}	
+}
+if ( $wp_query->max_num_pages > 1 ) : ?>
+		<div id="nav-bottom">
+			<span class="nav-previous"><?php next_posts_link( '&larr;上一頁' ); ?></span>
+			<span class="nav-next"><?php previous_posts_link( '下一頁&rarr;' ); ?></span>
+		</div>
+		<?php endif; ?>
+	</div><!--content-->
+</div><!--content-box-->
 <?php get_footer(); ?>
