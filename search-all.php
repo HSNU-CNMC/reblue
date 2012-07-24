eac85fba40e4c8e6a1db2ec51ad21e3f73438e30
Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
2012-07-24 05:36:30 +0000

    Merge from trunk

--- search-all.php
+++ search-all.php
@@ -5,36 +5,36 @@
 global $wpdb;
 
 // If no search type is specified, show search form
-if ( isset($_GET['st']) && !empty($_GET['st']) )
+if ( ( isset($_GET['type']) && !empty($_GET['type']) ) || ( isset($_GET['st']) && !empty($_GET['st']) ) )
 {
-	$st = $wpdb->escape($_GET['st']);
-	$sq = $wpdb->escape($_GET['sq']);// search query
-	$owner_query = $wpdb->escape($_GET['owner']);
+	$type = ( isset($_GET['type']) ) ? $wpdb->escape($_GET['type']) : $wpdb->escape($_GET['st']);// search type
+	$title_query = ( isset($_GET['title']) ) ? $wpdb->escape($_GET['title']) : $wpdb->escape($_GET['sq']);// title search keyword
+	$owner_query = $wpdb->escape($_GET['owner']);//owner search keyword
 
 	// Is this WP search or BT search?
-	if ( $st == 'bt' )
+	if ( $type == 'bt' )
 	{
 		get_header();
-		$sp = (empty($_GET['sp'])) ? '' : $wpdb->escape($_GET['sp']); // sp = search page
-		if ( !$sp ) $sp = 1;
+		$page = (empty($_GET['page'])) ? '' : $wpdb->escape($_GET['page']); // sp = search page
+		if ( !$page ) $page = 1;
 ?>
 <div class="content-box clear" >
 	<div class="content">
 <?php
 		// Search by title or by owner?
-		if ( isset($_GET['sq']) ) {
-			do_action('get_bt_search_by_title',$sq,$sp);
+		if ( isset($_GET['title']) ) {
+			do_action('get_bt_search_by_title',$title_query,$page);
 		} elseif ( isset($_GET['owner']) ) {
-			do_action('get_bt_search_by_owner', $owner_query, $sp);
+			do_action('get_bt_search_by_owner', $owner_query, $page);
 		}
 		?>
 	</div><!--content-->
 </div><!--content-box-->
 <?php  get_footer(); 
-	} elseif ( $st == 'wp' ) {
+	} elseif ( $type == 'wp' ) {
 		// Show WP's search result
 		$bg_url = get_bloginfo('wpurl');
-		header("Location: $bg_url/?s=$sq");
+		header("Location: $bg_url/?s=$title_query");// use title query as wordpress search keyword
 die;
 	}
 } else {
