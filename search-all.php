04bff0bde0af04a27fec98b66a89c7b1f1e95be9
Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
2012-08-29 08:01:50 +0000

    Use better selector to select <select>

--- search-all.php
+++ search-all.php
@@ -47,7 +47,7 @@ die;
 	<div class="content">
 		<form method="get" id="searchform" action="<?php bloginfo('url')?>/search">
 			<label id="type">搜尋目標
-				<select name="type" tabindex="3">
+				<select name="type" id="searchpage_select"tabindex="3">
 					<option value="bt">公告</option>
 					<option value="wp">文章</option>
 				</select>
@@ -58,9 +58,9 @@ die;
 	<script type="text/javascript">
 	function check_search_type() {
 		$('div#inserthere').empty();
-		if ( $('select option:selected:last').val() == 'bt' ) {
+		if ( $('select#searchpage_select option:selected').val() == 'bt' ) {
 			$('div#inserthere').append('<label>公告標題<input type="text" value="<?php the_search_query(); ?>" name="title" id="s" tabindex="1" /></label><label>公告單位<input type="text" value="" name="owner" id="o" tabindex="2" /></label><p>輸入一個條件即可，不會同時檢查兩個條件</p>');
-		} else if ( $('select option:selected:last').val() == 'wp' ) {
+		} else if ( $('select#searchpage_select option:selected').val() == 'wp' ) {
 			$('div#inserthere').append('<label>文章關鍵字<input type="text" value="" name="post" id="p" tabindex="1" /></label>');
 		}
 	}
