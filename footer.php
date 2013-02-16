c75dd44e00fdb79fadeb333fb3600b40beb2f584
Root Testing <pellaeon@hs.ntnu.edu.tw>
2013-02-16 17:45:29 +0800

    Adapt footer to use optiontree

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
-			<div class="footer">
+			<div class="footer clear text">
 				<div class="effect">
 					<div class="fcolumns_container text">
-						<?php	for ( $i = 1 ; $i <= 4 ; $i++ )
+						<?php	for ( $i = 1 ; $i <= 3 ; $i++ )
 						{
 							echo '<div class="fcol">';
-							echo pagelines('footer_col'.$i);
+							echo ot_get_option('footer_column_'.$i);
 							echo '</div>';
 						} ?>
 						<div class="fcol">
-						<?php echo pagelines('inside_contact_info'); ?>
+						<?php echo ot_get_option('footer_contact'); ?>
 						</div>
 					</div><!--fcolumns_container-->
+					<a href="#top" class="mobile-text">top</a>
 				</div><!--effect-->
 			</div><!--footer-->
 		</div><!--wrapper-->
